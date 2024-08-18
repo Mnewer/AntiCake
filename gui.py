e7d02bf63c11093105df819fac9ba32950719f2f
Jan Odvar Grindheim <47643578+Mnewer@users.noreply.github.com>
2024-08-18 12:39:50 +0200

    Some cleanup, added upload image function and button

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import filedialog
 from PIL import Image, ImageTk
 import os
 from utils import camera
@@ -8,40 +9,67 @@ class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
-        self.master.geometry("800x600")
-        self.cam = camera.Camera()
+        self.master.geometry("1000x400")
         self.pack()
         self.create_widgets()
         self.populate_image_list()
+        self.image_folder = "trainingImages"
+        self.images = []
+        # self.check_for_new_images()
 
     def create_widgets(self):
-        self.image_frame = tk.Frame(self)
-        self.control_frame = tk.Frame(self)
+        self.frame_image_interaction = tk.Frame(self)
 
-        self.image_frame.grid(row=0, column=0, rowspan=2, sticky="nsew")
-        self.control_frame.grid(row=0, column=1, rowspan=2, sticky="nsew")
+        self.frame_image_interaction.grid(row=0, column=0, rowspan=2, sticky="nsew")
 
-        self.image_preview = tk.Label(self.image_frame, text="Image Preview")
+        self.image_preview = tk.Label(self.frame_image_interaction, text="Image Preview")
         self.image_preview.grid(row=0, column=0, sticky="nsew")
 
-        self.navigation_frame = tk.Frame(self.image_frame)
-        self.navigation_frame.grid(row=1, column=0, sticky="ew")
+        # Create a frame for the navigation buttons
+        self.frame_navigation = tk.Frame(self.frame_image_interaction)
+        self.frame_navigation.grid(row=1, column=0, sticky="ew")
 
-        self.back_button = tk.Button(self.navigation_frame, text="Back", command=self.show_previous_image)
-        self.back_button.pack(side="left")
+        self.back_button = tk.Button(self.frame_navigation, text="Back", command=self.show_previous_image)
+        self.back_button.grid(row=0, column=0, sticky="w")
 
-        self.next_button = tk.Button(self.navigation_frame, text="Next", command=self.show_next_image)
-        self.next_button.pack(side="right")
+        self.upload_button = tk.Button(self.frame_navigation, text="Upload", command=self.upload_image)
+        self.upload_button.grid(row=0, column=1, sticky="w")
 
+        self.next_button = tk.Button(self.frame_navigation, text="Next", command=self.show_next_image)
+        self.next_button.grid(row=0, column=2, sticky="w")
+
+        # Create a listbox to display the image files
         self.image_list = tk.Listbox(self)
-        self.image_list.grid(row=0, column=1, sticky="nsew")
+        self.image_list.grid(row=0, column=1, rowspan=2, sticky="nsew")
         self.image_list.bind("<<ListboxSelect>>", self.on_image_select)
 
-        self.extra_frame = tk.Frame(self)
-        self.extra_frame.grid(row=0, column=2, rowspan=2, sticky="nsew")
 
-        self.quit = tk.Button(self.extra_frame, text="QUIT", fg="red", command=self.master.destroy)
+        # Controls frame:
+        self.frame_controls = tk.Frame(self)
+        self.frame_controls.grid(row=0, column=2, rowspan=2, sticky="nsew", padx=10)
+
+        self.capture_button = tk.Button(self.frame_controls, text="Capture images", command=self.capture_image)
+        self.capture_button.pack(side="top", pady=5, anchor="w")
+
+        self.remove_unclear_button = tk.Button(self.frame_controls, text="Remove unclear images")
+        self.remove_unclear_button.pack(side="top", pady=5, anchor="w")
+
+        self.prep_images_button = tk.Button(self.frame_controls, text="Prepare images")
+        self.prep_images_button.pack(side="top", pady=5, anchor="w")
+
+        self.train_button = tk.Button(self.frame_controls, text="Train model")
+        self.train_button.pack(side="top", pady=5, anchor="w")
+
+        self.quit = tk.Button(self.frame_controls, text="QUIT", fg="red", command=self.master.destroy)
         self.quit.pack(side="bottom")
+
+        self.threshold_input = tk.Entry(self.frame_controls, width=10)
+        self.threshold_label = tk.Label(self.frame_controls, text="Threshold for unclear images:")
+        self.threshold_label.pack(side="top", pady=5, anchor="w")
+
+        self.threshold_input.insert(0, "500")
+        self.threshold_input.pack(side="top", pady=5, anchor="w")
+        
 
         # Configure grid weights to make the layout responsive
         self.master.grid_rowconfigure(0, weight=1)
@@ -50,6 +78,12 @@ class Application(tk.Frame):
         self.master.grid_columnconfigure(1, weight=1)
         self.master.grid_columnconfigure(2, weight=1)
 
+    # MARK: Capture image
+    def capture_image(self):
+        cam = camera.Camera()
+        cam.capture_image()
+
+    # MARK: Populate image list
     def populate_image_list(self):
         """
         Populates the image list in the GUI with image files from the specified folder.
@@ -79,6 +113,33 @@ class Application(tk.Frame):
                 self.current_image_index = 0
                 self.show_image(self.images[self.current_image_index])
 
+    # MARK: Upload image
+    def upload_image(self):
+        """
+        Opens a file dialog to select an image file and copies it to the trainingImages folder.
+        Updates the image list, appends the image to the images list, and shows the selected image.
+
+        Returns:
+            None
+        """
+        file_path = filedialog.askopenfilename(title="Select an image file", filetypes=[("Image files", "*.png *.jpg *.jpeg *.gif *.bmp")])
+        if file_path:
+            file_name = os.path.basename(file_path)
+            image_folder = "trainingImages"
+            image_path = os.path.join(image_folder, file_name)
+            # copy the selected image to the trainingImages folder
+            try:
+                os.makedirs(image_folder, exist_ok=True)
+                os.replace(file_path, image_path)
+                self.image_list.insert(tk.END, file_name)
+                self.images.append(file_name)
+                self.current_image_index = len(self.images) - 1
+                self.show_image(file_name)
+            except OSError as e:
+                print(f"Error moving file: {e}")
+    
+
+    # MARK: Image selection
     def on_image_select(self, event):
         """
         Event handler for when an image is selected from the image list.
@@ -95,6 +156,8 @@ class Application(tk.Frame):
             selected_image = self.image_list.get(selected_index)
             self.show_image(selected_image)
 
+
+    # MARK: Show image
     def show_image(self, image_name):
         """
         Displays the specified image in the image preview box.
@@ -119,6 +182,7 @@ class Application(tk.Frame):
         self.image_list.selection_set(self.current_image_index)
         self.image_list.see(self.current_image_index)
 
+    # MARK: Show previous and next image
     def show_previous_image(self):
         """
         Displays the previous image in the list of images.
